263ef17cac004e80b80900782a94ac66fe43d722
itmo338951 <338951@niuitmo.ru>
2024-11-14 16:36:28 +0300

    Yet again fixes

--- main.js
+++ main.js
@@ -4,8 +4,9 @@ window.addEventListener("load", function () {
     const loadStatElement = document.getElementById("js-loading-time");
     loadStatElement.innerText = String(Date.now() - loadStart);
 
-    for (const element of document.querySelectorAll(".header-navbar-item > a")) {
-        const href = element.dataset["ref"];
+    for (const element of document.querySelectorAll(".header-navbar-item")) {
+        const hrefElement = element.querySelector("a");
+        const href = hrefElement.href;
         if (href && location.pathname === href) {
             element.classList.add("header-navbar-item-active");
         }
